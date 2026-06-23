41ff13f66812992b299da557fc605208ef5be695
Jerry Huang <jerry@conic-soft.com>
2026-06-23 09:47:32 +0800

    Day2

---
+++ Day2/CS_Solution/Ap01/Program.cs
@@ -0,0 +1,9 @@
+﻿string s;
+int score;
+
+Console.Write("Score:");
+s = Console.ReadLine();
+score = int.Parse(s);
+
+Console.WriteLine(score < 60 ? "不及格" : "及格");
+Console.WriteLine(score < 60 ? "C-Class" : score < 80 ? "B-Class" : "A-Class");
---
+++ Day2/CS_Solution/Ap02/Program.cs
@@ -0,0 +1,32 @@
+﻿string s;
+int n;
+
+Console.Write("n=");
+s = Console.ReadLine();
+n = int.Parse(s);
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= i; j++)
+        Console.Write("*");
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = n; i >= 1; i--)
+{
+    for (int j = 1; j <= i; j++)
+        Console.Write("*");
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n - i; j++)
+        Console.Write(" ");
+    for (int j = 1; j <= 2 * i - 1; j++)
+        Console.Write("*");
+    Console.WriteLine();
+}
