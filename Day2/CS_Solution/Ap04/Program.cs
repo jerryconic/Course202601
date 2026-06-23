d4af32775b0dda6cd9e4d2fb956d01acbec03cba
Jerry Huang <jerry@conic-soft.com>
2026-06-23 10:18:21 +0800

    Local Function

---
+++ Day2/CS_Solution/Ap04/Program.cs
@@ -0,0 +1,19 @@
+﻿SayHello();
+SayHello();
+SayHello();
+
+Console.WriteLine(AddNumber(10, 20));
+Console.WriteLine(AddNumber(50, 60));
+
+void SayHello()
+{
+    Console.WriteLine("Hello");
+    //return;
+}
+
+int AddNumber(int a, int b)
+{
+    Console.WriteLine($"a={a}, b={b}");
+    return a + b;
+}
+
