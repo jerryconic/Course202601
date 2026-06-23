cd4de9dd21dce8c1c35801519381a274adafc542
Jerry Huang <jerry@conic-soft.com>
2026-06-23 14:58:55 +0800

    Class Person

---
+++ Day2/CS_Solution/Ap17/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap17
+{
+    internal class Person
+    {
+        private string _id, _name;
+        public void Display()
+        {
+            Console.WriteLine($"Id={_id}, Name={_name}");
+        }
+        public Person(string id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+    }
+
+}
---
+++ Day2/CS_Solution/Ap17/Program.cs
@@ -0,0 +1,5 @@
+﻿using Ap17;
+
+Person p1 = new Person("C001", "John");
+
+p1.Display();
