d5cb4ce3d1cacf1beea44b034647ab65271958d0
Jerry Huang <jerry@conic-soft.com>
2026-06-23 15:22:39 +0800

    BMI Calculator

---
+++ Day2/CS_Solution/Ap19/BMICalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap19
+{
+    internal class BMICalculator
+    {
+        private double _height, _weight;
+
+        public string Status()
+        {
+            double bmi = _weight / _height / _height;
+            if (bmi < 18.5)
+                return string.Format($"BMI={bmi:0.00}, 體重過輕");
+            else if (bmi < 24)
+                return string.Format($"BMI={bmi:0.00}, 體重正常");
+            else
+                return string.Format($"BMI={bmi:0.00}, 體重過重");
+
+        }
+        /// <summary>
+        /// BMI計算器
+        /// </summary>
+        /// <param name="height">身高(cm)</param>
+        /// <param name="weight">體重(kg)</param>
+        public BMICalculator(double height, double weight)
+        {
+            _height = height / 100;
+            _weight = weight;
+        }
+    }
+}
---
+++ Day2/CS_Solution/Ap19/Program.cs
@@ -0,0 +1,4 @@
+﻿using Ap19;
+
+BMICalculator calc = new BMICalculator(180, 100);
+Console.WriteLine(calc.Status());
