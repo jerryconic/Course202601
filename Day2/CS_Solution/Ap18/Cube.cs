bb7b3aa62ae0219bb3f1d4aff04b904bd4d2a8df
Jerry Huang <jerry@conic-soft.com>
2026-06-23 15:08:01 +0800

    Cube

---
+++ Day2/CS_Solution/Ap18/Cube.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap18
+{
+    internal class Cube
+    {
+        private double _length, _width, _height;
+        /// <summary>
+        /// 建構函式
+        /// </summary>
+        /// <param name="length">長</param>
+        /// <param name="width">寬</param>
+        /// <param name="height">高</param>
+        public Cube(double length, double width, double height)
+        {
+            _length = length;
+            _width = width;
+            _height = height;
+        }
+
+        /// <summary>
+        /// 體積
+        /// </summary>
+        /// <returns>長X寬X高</returns>
+        public double Volume() 
+        { 
+            return _length * _width * _height; 
+        }
+        /// <summary>
+        /// 表面積
+        /// </summary>
+        /// <returns></returns>
+        public double Area() 
+        { 
+            return 2 * (_length * _width + _length * _height + _width * _height); 
+        }
+    }
+}
---
+++ Day2/CS_Solution/Ap18/Program.cs
@@ -0,0 +1,7 @@
+﻿using Ap18;
+
+Cube c1 = new Cube(6,6,6);
+Console.WriteLine($"c1: Area={c1.Area()}, Volume={c1.Volume()}");
+
+Cube c2 = new Cube(12, 5, 10);
+Console.WriteLine($"c2: Area={c2.Area()}, Volume={c2.Volume()}");
