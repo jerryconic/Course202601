df50ecb3b284a3143071860eb5a75891c875f10f
Jerry Huang <jerry@conic-soft.com>
2026-06-23 15:36:51 +0800

    GuessGame

---
+++ Day2/CS_Solution/Ap20/GuessGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap20
+{
+    internal class GuessGame
+    {
+        private int _guess, _ans;
+        public void Start()
+        {
+            Random rnd = new Random();
+            _ans = rnd.Next(1, 100);
+            //Console.WriteLine(_ans);
+        }
+        public void Guess()
+        {
+            string s;
+            Console.Write("猜數字(1-99):");
+            s = Console.ReadLine();
+            int.TryParse(s, out _guess);
+        }
+
+        public bool GotAns()
+        {
+            if (_guess < _ans)
+                Console.WriteLine("高一點");
+            else if (_guess > _ans)
+                Console.WriteLine("低一點");
+            else
+                Console.WriteLine("猜對了");
+            return _ans == _guess;
+            /*
+            if (_ans == _guess)
+                return true;
+            else
+                return false;
+            */
+        }
+    }
+}
---
+++ Day2/CS_Solution/Ap20/Program.cs
@@ -0,0 +1,8 @@
+﻿using Ap20;
+
+GuessGame g1 = new GuessGame();
+g1.Start();
+do
+{
+    g1.Guess();
+} while (!g1.GotAns());
