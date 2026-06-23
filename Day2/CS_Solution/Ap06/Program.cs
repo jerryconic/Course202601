82ceba8c77054eb021c7b126fd3fcdaa5dd96f35
Jerry Huang <jerry@conic-soft.com>
2026-06-23 10:41:27 +0800

    Menu

---
+++ Day2/CS_Solution/Ap06/Program.cs
@@ -0,0 +1,60 @@
+﻿for(; ; )
+{
+    switch(Choice())
+    {
+        case "1":
+            CalcScore();
+            break;
+        case "2":
+            CalcTax();
+            break;
+        case "3":
+            Exchange();
+            break;
+        case "4":
+            DrawStar();
+            break;
+        case "5":
+            CalcInterest();
+            break;
+        case "0":
+            Console.WriteLine("bye.");
+            return;
+    }
+    Console.Write("Press any key to continue...");
+    Console.ReadKey();
+}
+string Choice()
+{
+    Console.Write(@"
+1.成績計算
+2.計算所得稅
+3.匯率換算
+4.畫星星
+5.計算利息
+0.離開
+請選擇(0-5):");
+    return Console.ReadLine();
+}
+
+void CalcScore()
+{
+    
+}
+void CalcTax()
+{
+
+}
+void Exchange()
+{
+
+}
+
+void DrawStar()
+{
+
+}
+void CalcInterest()
+{
+
+}
