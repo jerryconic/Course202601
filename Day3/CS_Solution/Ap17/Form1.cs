0d6b4f5a98c3cb1fd42d2202cb63222701704632
Jerry Huang <jerry@conic-soft.com>
2026-06-24 16:24:15 +0800

    KeyDown

---
+++ Day3/CS_Solution/Ap17/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ap17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode) 
+            {
+                case Keys.Down:
+                case Keys.Return:
+                    e.SuppressKeyPress = true;
+                    SendKeys.Send("{Tab}"); 
+                    break; 
+                case Keys.Up: 
+                    SendKeys.Send("+{Tab}"); 
+                    break; 
+            }
+        }
+
+        private void txt_Enter(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            txt.SelectAll();
+        }
+    }
+}
