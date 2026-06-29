4a9a84521e37e5f0d6cf06d0e8d72570f408f806
Jerry Huang <jerry@conic-soft.com>
2026-06-29 15:32:24 +0800

    Web_MVC

---
+++ Day5/WebAp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAp.Models;
+
+namespace WebAp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["MyName"] = "John";
+            ViewData["NowTime"] = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Day5/WebAp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
